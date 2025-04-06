6218f8d8235bca5535a997f26ddfd5038d10d595
Mario <mariohertu@gmail.com>
2025-04-06 16:25:29 -0600

    Added information page for vtt processing

--- src/webserver/app.py
+++ src/webserver/app.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, UploadFile, File, BackgroundTasks
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from src.parser import extract_data
+from pathlib import Path
+import os
+import asyncio
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="src/webserver/static"), name="static")
-
+app.mount("/temp", StaticFiles(directory="src/webserver/temp"), name="temp")
 
 templates = Jinja2Templates(directory="src/webserver/templates")
+
+async def delete_temp_file(file_name: str):
+    await asyncio.sleep(600)
+    try:
+        os.remove(f"src/webserver/temp/{file_name}")
+    except:
+        print('Could not remove temp file')
+    else:
+        print('Temp file removed')
 
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
@@ -18,6 +31,37 @@ async def main(request: Request):
 async def upload_document(request: Request):
     return templates.TemplateResponse(request=request, name='upload-document.html')
 
+@app.post('/upload-document')
+async def hanlde_file(request: Request,file: UploadFile):
+    with open(f'src/webserver/temp/{file.filename}', 'wb') as f:
+        f.write(await file.read())
+    return RedirectResponse(f'/document-info/{file.filename}', status_code=303)
+
+@app.get('/document-info/{file_name}')
+async def document_info(request: Request, file_name: str, background_tasks: BackgroundTasks):
+    _file = Path(f"src/webserver/temp/{file_name}")
+    content_iterator = extract_data(_file)
+    items = extract_data(_file)
+    speakers = set()
+    for item in content_iterator:
+        speakers.add(item.speaker.title())
+    ctx = {
+        'file_name':file_name,
+        'speakers':speakers,
+        'items':items
+    }
+    background_tasks.add_task(delete_temp_file, file_name)
+    return templates.TemplateResponse(request=request, name='document-info.html', context=ctx)
+
+@app.delete('delete-temp/{file_name}')
+async def delete_temp(request: Request, file_name: str):
+    try:
+        os.remove(f"src/webserver/temp/{file_name}")
+    except:
+        print('Could not remove temp file')
+    else:
+        print('Temp file removed')
+    return
 
 if __name__ == '__main__':
     import uvicorn
--- src/webserver/static/js/index.js
+++ src/webserver/static/js/index.js
@@ -1,8 +1,16 @@
 const app = document.getElementById('app');
 const msg = document.getElementById('msg');
+const input = document.getElementById('file');
 
 app.addEventListener('click', function (event) {
     if (event.target && event.target.id === "submit_vtt") {
         console.log("Button clicked!");
     }
 });
+
+input.addEventListener("input", updateValue);
+
+function updateValue(e) {
+    console.log(e.target.value);
+}
+
